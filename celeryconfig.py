d9a747c663ce0b24fa0865b526156611a3d5e30c
Senanyo Benwari <65190458+nightlyhawk@users.noreply.github.com>
2024-08-15 10:28:05 +0100

    Update celeryconfig.py

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,8 +1,8 @@
 import os
 
-host = os.environ('UPSTASH_REDIS_HOST')
-password = os.environ('UPSTASH_REDIS_PASSWORD')
-port = os.environ('UPSTASH_REDIS_PORT')
+# host = os.environ('UPSTASH_REDIS_HOST')
+# password = os.environ('UPSTASH_REDIS_PASSWORD')
+# port = os.environ('UPSTASH_REDIS_PORT')
 
-broker_url = "redis://:{}@{}:{}".format(password, host, port)
-result_backend = "redis://:{}@{}:{}".format(password, host, port)
+broker_url =os.getenv('BROKER_URL')
+result_backend = os.getenv('RESULT_BACKEND')

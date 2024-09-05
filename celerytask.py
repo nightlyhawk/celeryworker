1796c61722ea5a78cc8b06f3a616aed4671b7683
Senanyo Benwari <65190458+nightlyhawk@users.noreply.github.com>
2024-09-05 14:41:18 +0100

    Update celerytask.py

--- celerytask.py
+++ celerytask.py
@@ -3,3 +3,8 @@ from celery import Celery
 app = Celery("task")
 app.config_from_object("celeryconfig")
 app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
